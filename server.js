4fa192f6f0c512b83ed91f1ab2aa675cb6811721
Jason Dryhurst-Smith <jason@jasonds.co.uk>
2016-07-16 16:27:51 +0100

    get port from env

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
-app.use(express.static('app'));
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'app')));
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+	console.log('Example app listening on port %s!', app.get('port'));
 });
