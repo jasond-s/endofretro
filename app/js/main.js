94afe60d37266d35120a50513f6e195582cc6c38
Jason Dryhurst-Smith <jason@jasonds.co.uk>
2016-07-16 16:43:14 +0100

    move everything into the right files

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,37 @@
 
+(function () {
+	var animon = true;
+
+	function justStopIt() {
+		$('.wrap').toggleClass('stop');
+		$('.wrap-anim').toggleClass('stop');
+
+		if (animon)
+		  $('.cube-anim span').text('turn animation on');
+		else 
+		  $('.cube-anim span').text('turn animation off');
+
+		animon = !animon;
+	}
+
+	function bringIntoView(goTo) {  
+		var elem = $(goTo);
+		var offset = elem.offset();
+		var top = offset.top;
+
+		$("html,body").animate({ scrollTop: top }, 2000); 
+	}
+
+	function goToMusic () {
+		bringIntoView('body');
+	}
+	function goToLive () {
+		bringIntoView('#live');
+	}
+	function goToSocial () {
+		bringIntoView('#social');
+	}
+	function goToContact () {
+		bringIntoView('#contact');
+	}
+}())
